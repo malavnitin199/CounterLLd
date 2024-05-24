76d801b4e9a53a96ff77f5a4d84ed9c9fe98ac9c
malavnitin199 <91662760+malavnitin199@users.noreply.github.com>
2024-05-24 13:23:34 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./components/Homepage/Homepage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InputForm/InputForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./InputForm.css"; // Import the CSS file
+
+// eslint-disable-next-line react/prop-types
+const InputForm = ({ onDateSelect, onCancel, isCountDownActive }) => {
+  const [date, setDate] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onDateSelect(date);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="datetime-local"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        required
+        className="input-field"
+      />
+      {!isCountDownActive && (
+        <button type="submit" className="button">
+          Start Timer
+        </button>
+      )}
+      {isCountDownActive && (
+        <button type="button" onClick={onCancel} className="button">
+          Cancel Timer
+        </button>
+      )}
+    </form>
+  );
+};
+
+export default InputForm;
